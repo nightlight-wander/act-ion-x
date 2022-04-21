a520b2e3d1f2f543d9afaabd6bec594f12fdb50b
nisha4545 <88261286+nisha4545@users.noreply.github.com>
2022-04-21 22:07:03 +0530

    video data counts increased

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -32,6 +32,15 @@ export const videos = [
   },
 
   {
+    _id: "Mi6VE77Hpbk",
+    category: "Sci-Code",
+    genreName:"Cyborgs Science",
+    title: "Swinging from web shooters",
+    creator: "HackSmith Industeries",
+    thumbnail:"https://picsum.photos/id/1/200/300",
+  },
+
+  {
     _id: "aF4DoCY0cNg",
     category: "Sci-Code",
     genreName:"Cyborgs Science",
@@ -46,6 +55,60 @@ export const videos = [
     genreName:"Cyborgs Science",
     title: "Vehicle powered by wind,outruns wind",
     creator: "Xyla Foxlin",
+    thumbnail:"https://picsum.photos/id/1/200/300",
+  },
+
+  {
+    _id: "HeQX2HjkcNo",
+    category: "Sci-Code",
+    genreName:"Predictions Science",
+    title: "Math's Fundamental Law",
+    creator: "Veritasium",
+    thumbnail:"https://picsum.photos/id/1/200/300",
+  },
+
+  {
+    _id: "j4Fh9L7iCBY",
+    category: "Sci-Code",
+    genreName:"Predictions Science",
+    title: "Biology an expression of field",
+    creator: "Nassim Haramein",
+    thumbnail:"https://picsum.photos/id/1/200/300",
+  },
+
+  {
+    _id: "xJsl_klqVh0",
+    category: "Sci-Code",
+    genreName:"Predictions Science",
+    title: "Connected universe ted",
+    creator: "Nassim Haramein",
+    thumbnail:"https://picsum.photos/id/1/200/300",
+  },
+
+  {
+    _id: "4b33NTAuF5E",
+    category: "Sci-Code",
+    genreName:"Predictions Science",
+    title: "Upload your mind and live forever?No!",
+    creator: "Kurzgesagt",
+    thumbnail:"https://picsum.photos/id/1/200/300",
+  },
+
+  {
+    _id: "La9oLLoI5Rc",
+    category: "Sci-Code",
+    genreName:"Quantas Science",
+    title: "Accelarating towards full potential joe dispenza",
+    creator: "Tom Bileyu",
+    thumbnail:"https://picsum.photos/id/1/200/300",
+  },
+
+  {
+    _id: "Uklt4zVVtS0",
+    category: "Sci-Code",
+    genreName:"Quantas Science",
+    title: "Stay healthy until 105 steven gundry",
+    creator: "Tom Bileyu",
     thumbnail:"https://picsum.photos/id/1/200/300",
   },
 
--- src/components/BottomNav/BottomNav.jsx
+++ src/components/BottomNav/BottomNav.jsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import MovieIcon from '@mui/icons-material/Movie';
-import TvIcon from '@mui/icons-material/Tv';
-import WhatshotIcon from '@mui/icons-material/Whatshot';
-import "./BottomNav.css";
+// import React from 'react';
+// import { Link } from 'react-router-dom';
 
-export function BottomNav() {
-  return (
-    <nav className="actions-cat flex-center">
-      <Link to="/" className="flex-col flex-vCenter">
-        <span><WhatshotIcon /></span>
-        <span className="actions-name">Home</span>
-      </Link>
-      <Link to="/movies" className="flex-col flex-vCenter">
-        <span><MovieIcon /></span>
-        <span className="actions-name">Movies</span>
-      </Link>
-      <Link to="/series" className="flex-col flex-vCenter">
-        <span><TvIcon /></span>
-        <span className="actions-name">Series</span>
-      </Link >
-    </nav>
+// import "./BottomNav.css";
 
-  );
-}
+// export function BottomNav() {
+//   return (
+//     <nav className="actions-cat flex-center">
+//       <Link to="/" className="flex-col flex-vCenter">
+//         <span></span>
+//         <span className="actions-name">Home</span>
+//       </Link>
+//       <Link to="/" className="flex-col flex-vCenter">
+//         <span></span>
+//         <span className="actions-name"></span>
+//       </Link>
+//       <Link to="/" className="flex-col flex-vCenter">
+//         <span></span>
+//         <span className="actions-name"></span>
+//       </Link >
+//     </nav>
+
+//   );
+// }
