651a1313936b4e2a9a747b8f79b791b922114307
nisha4545 <88261286+nisha4545@users.noreply.github.com>
2022-05-04 12:14:20 +0530

    refactor:video card

---
+++ src/components/VideoCard/VideoCard.jsx
@@ -0,0 +1,23 @@
+import VideosStyles from "../../pages/VideoListing/VideoListing.module.css";
+
+const VideoCard = ({ videoObj }) => {
+    return (
+        <div className={`${VideosStyles.videoWrapper}`}>
+            <div className={`${VideosStyles.videoBackground}`}><img src={videoObj.thumbnail} alt={videoObj.title} className={`${VideosStyles.videoImg}`}></img></div>
+            <div className={`${VideosStyles.videoTitle}`}><h2>{videoObj.title}</h2></div>
+            <span className={`${VideosStyles.videoCreator}`}>{videoObj.creator}</span>
+            <div className={`${VideosStyles.videoIcon}`}>
+                <span className={` material-icons material-icons-outlined`}>
+                    watch_later
+                </span>
+                <span className={`material-icons`}>
+                    favorite_border
+                </span>
+            </div>
+
+        </div>
+
+    )
+}
+
+export { VideoCard };
--- src/pages/VideoListing/VideoListing.jsx
+++ src/pages/VideoListing/VideoListing.jsx
@@ -5,41 +5,31 @@ import { Header } from "../../components/Header/Header";
 import { useVideos } from "../../context/VideosContest";
 import VideosStyles from "./VideoListing.module.css";
 import { fetchVideosByCat } from "../../services/videosCatsServices";
+import { VideoCard } from "../../components/VideoCard/VideoCard";
 
 const VideoListing = () => {
-  const {videoDispatch,videoStates:{category,selectGenre,isGenres,videos}} = useVideos();
+  const { videoDispatch, videoStates: { category, selectGenre, isGenres, videos } } = useVideos();
 
-  useEffect(()=>{
-    (fetchVideosByCat())(videoDispatch,category,selectGenre,isGenres);
+  useEffect(() => {
+    (fetchVideosByCat())(videoDispatch, category, selectGenre, isGenres);
     // eslint-disable-next-line
-},[category,isGenres,selectGenre])
+  }, [category, isGenres, selectGenre])
 
   return (
     <>
       <Header />
       <div className={`${VideosStyles.videosWrapper}`}>
-        {videos.map((videoObj) => {
-          return <div className={`${VideosStyles.videoWrapper}`}>
-            <div className={`${VideosStyles.videoBackground}`}><img src={videoObj.thumbnail} alt={videoObj.title} className={`${VideosStyles.videoImg}`}></img></div>
-            <div className={`${VideosStyles.videoTitle}`}><h2>{videoObj.title}</h2></div>
-            <span className={`${VideosStyles.videoCreator}`}>{videoObj.creator}</span>
-            <div className={`${VideosStyles.videoIcon}`}>
-            <span className={` material-icons material-icons-outlined`}>
-              watch_later
-            </span>
-            <span className={`material-icons`}>
-              favorite_border
-            </span>
-            </div>
-            
-          </div>
-        })}
+        {videos.map((videoObj) => (
+          <VideoCard videoObj={videoObj}
+          />
+        ))}
       </div>
-      <GenreSlider/>
-      <BottomNav/>
+      <GenreSlider />
+      <BottomNav />
     </>
 
   )
 }
 
 export { VideoListing };
+
