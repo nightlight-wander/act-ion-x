d422697f8d74c7747aad9af6866eabc12bd1176a
nisha4545 <88261286+nisha4545@users.noreply.github.com>
2022-05-21 15:31:54 +0530

    refactor links to navlinks

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import HeaderStyles from "./Header.module.css";
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { useAuth } from '../../context/AuthContext';
 import { useState,useEffect } from "react";
 import { useVideos } from "../../context/VideosContest";
@@ -38,7 +38,7 @@ const Header = () => {
   return (
     <header className={`${HeaderStyles.header}`}>
       <div className={`${HeaderStyles["logo-head"]} flex-vCenter`} >
-        <Link to="/" className={`${HeaderStyles["logo-name"]}`}>ACT-IONS</Link>
+        <NavLink to="/" className={`${HeaderStyles["logo-name"]}`}>ACT-IONS</NavLink>
       </div>
       <div className={`${HeaderStyles["search-bar"]}`}>
         <input type="text" className={`${HeaderStyles["search-input"]}`} placeholder="Search" aria-label="search" onChange={(e)=>updateDebounceText(e.target.value)}/>
@@ -49,42 +49,54 @@ const Header = () => {
 
       <div className={`${HeaderStyles["video-actions"]} flex-vCenter`}>
         
-        <Link to="/playlists" className={`flex-col flex-vCenter`}>
+        <NavLink to="/playlists" className={`flex-col flex-vCenter`} style={({isActive}) => ({
+    color: isActive ? `var(--light-red)` : ""
+  })}>
           <span class="material-icons">
             playlist_add
           </span>
           <span className={`${HeaderStyles["video-actions-title"]}`}>Playlist</span>
-        </Link>
-        <Link to="/watch-later" className={`flex-col flex-vCenter`}>
+        </NavLink>
+        <NavLink to="/watch-later" className={`flex-col flex-vCenter`} style={({isActive}) => ({
+    color: isActive ? `var(--light-red)` : ""
+  })}>
           <span class="material-icons">
             watch_later
           </span>
           <span className={`${HeaderStyles["video-actions-title"]}`}>Watch Later</span>
-        </Link>
-        <Link to="/history" className={`flex-col flex-vCenter`}>
+        </NavLink>
+        <NavLink to="/history" className={`flex-col flex-vCenter`} style={({isActive}) => ({
+    color: isActive ? `var(--light-red)` : ""
+  })}>
           <span class=" material-icons material-icons-outlined">
             history
           </span>
           <span className={`${HeaderStyles["video-actions-title"]}`}>History</span>
-        </Link>
-        <Link to="/liked" className={`flex-col flex-vCenter`}>
+        </NavLink>
+        {/* <NavLink to="/liked" className={`flex-col flex-vCenter`} style={({isActive}) => ({
+    color: isActive ? `var(--light-red)` : ""
+  })}>
           <span class="material-icons material-icons-outlined">
             favorite_border
           </span>
           <span className={`${HeaderStyles["video-actions-title"]}`}>Liked</span>
-        </Link>
-        {!eToken ? <Link to="/login" className={`flex-col flex-vCenter`}>
+        </NavLink> */}
+        {!eToken ? <NavLink to="/login" className={`flex-col flex-vCenter`} style={({isActive}) => ({
+    color: isActive ? `var(--light-red)` : ""
+  })}>
           <span class="material-icons">
             perm_identity
           </span>
           <span className={`${HeaderStyles["video-actions-title"]}`}>Login</span>
-        </Link> :
-        <Link to="/profile" className={`flex-col flex-vCenter`}>
+        </NavLink> :
+        <NavLink to="/profile" className={`flex-col flex-vCenter`} style={({isActive}) => ({
+          color: isActive ? `var(--light-red)` : ""
+        })}>
           <span class="material-icons">
             perm_identity
           </span>
           <span className={`${HeaderStyles["video-actions-title"]}`}>Profile</span>
-        </Link>
+        </NavLink>
         }
        
       </div>
